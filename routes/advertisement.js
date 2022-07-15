04c5d5dfef4223a85d838bfdfcbf085d23aa51ad
Kiran Sahmbi <kiransahmbi@yahoo.ca>
2022-07-15 14:54:17 -0400

    Fixed Error

--- routes/advertisement.js
+++ routes/advertisement.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+let advertisementController = require('../controllers/advertisement');
+
 // Get Advertisement List
 router.get('/list', advertisementController.advertisementList);
 
