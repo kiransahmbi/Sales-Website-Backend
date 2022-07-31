07390d693f2c6f9f0b6422411bc978eaccf51138
ahnaf79 <ahnaf.gdis2000@gmail.com>
2022-07-31 06:55:52 +0600

    fixing error

--- controllers/index.js
+++ controllers/index.js
@@ -5,23 +5,23 @@ exports.home = function(req, res, next) {
     res.redirect('/advertisement/list');
 };
 
-// exports.aboutUs = function(req, res, next) {
-//     res.render('aboutus', {
-//         title: 'About Us'
-//     });
-// };
+exports.aboutUs = function(req, res, next) {
+    res.render('aboutus', {
+        title: 'About Us'
+    });
+};
 
-// exports.users = function(req, res, next) {
-//     res.render('signup', {
-//         title: 'Sign-up',
-//         userName: req.user ? req.user.username : ''
-//     });
-// }
+exports.users = function(req, res, next) {
+    res.render('signup', {
+        title: 'Sign-up',
+        userName: req.user ? req.user.username : ''
+    });
+}
 
-// exports.signin = function(req, res, next) {
-//     res.render('signin', {
-//         title: 'Sign-in',
-//         userName: req.user ? req.user.username : ''
+exports.signin = function(req, res, next) {
+    res.render('signin', {
+        title: 'Sign-in',
+        userName: req.user ? req.user.username : ''
 
-//     });
-// }
+    });
+}
