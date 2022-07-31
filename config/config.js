f23612e88a171e7248a66e4e80b150140c5502c0
ahnaf79 <ahnaf.gdis2000@gmail.com>
2022-07-31 06:25:43 +0600

    adds dotenv

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
 // Secrect key needs to be edited
+require('dotenv').config();
+
 module.exports = {
-    "ATLASDB": "mongodb+srv://group4Admin:CentennialCollegeSummer2022@cluster003-group4.v8oxwbz.mongodb.net/Group4?retryWrites=true&w=majority",    
-    "SECRETKEY": "y$B&E)H+MbQeThWmZq4t7w!z%C*F-JaNcRfUjXn2r5u8x/A?D(G+KbPeSgVkYp3s"
+    "ATLASDB": process.env.ATLASDB,
+    "SECRETKEY": process.env.SECRETKEY
 }
--- controllers/index.js
+++ controllers/index.js
@@ -1,26 +1,27 @@
 exports.home = function(req, res, next) {
-    res.render('index', { 
-        title: 'Home'
-    });
+    // res.render('index', { 
+    //     title: 'Home'
+    // });
+    res.redirect('/advertisement/list');
 };
 
-exports.aboutUs = function(req, res, next) {
-    res.render('aboutus', { 
-        title: 'About Us'
-     });
-};
+// exports.aboutUs = function(req, res, next) {
+//     res.render('aboutus', {
+//         title: 'About Us'
+//     });
+// };
 
-exports.users = function(req, res, next) {
-    res.render('signup', { 
-      title: 'Sign-up',
-      userName: req.user ? req.user.username : ''
-    });
-}
+// exports.users = function(req, res, next) {
+//     res.render('signup', {
+//         title: 'Sign-up',
+//         userName: req.user ? req.user.username : ''
+//     });
+// }
 
-exports.signin = function(req, res, next) {
-    res.render('signin', { 
-      title: 'Sign-in',
-      userName: req.user ? req.user.username : ''
-      
-    });
-}
+// exports.signin = function(req, res, next) {
+//     res.render('signin', {
+//         title: 'Sign-in',
+//         userName: req.user ? req.user.username : ''
+
+//     });
+// }
