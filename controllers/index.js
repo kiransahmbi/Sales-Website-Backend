3e018fc6ad59ac8a4922986e4ad8773d1484d006
GurleenButtar <gbuttar8@my.centennialcollege.ca>
2022-07-30 21:55:46 -0400

    updated Index
    commented out res.render

--- controllers/index.js
+++ controllers/index.js
@@ -1,26 +1,26 @@
 exports.home = function(req, res, next) {
-    res.render('index', { 
-        title: 'Home'
-    });
+   // res.render('index', { 
+ //       title: 'Home'
+ //   });
 };
 
 exports.aboutUs = function(req, res, next) {
-    res.render('aboutus', { 
-        title: 'About Us'
-     });
+  //  res.render('aboutus', { 
+ //       title: 'About Us'
+  //   });
 };
 
 exports.users = function(req, res, next) {
-    res.render('signup', { 
-      title: 'Sign-up',
-      userName: req.user ? req.user.username : ''
-    });
+ //   res.render('signup', { 
+ //     title: 'Sign-up',
+  //    userName: req.user ? req.user.username : ''
+//    });
 }
 
 exports.signin = function(req, res, next) {
-    res.render('signin', { 
-      title: 'Sign-in',
-      userName: req.user ? req.user.username : ''
+ //   res.render('signin', { 
+  //    title: 'Sign-in',
+   //   userName: req.user ? req.user.username : ''
       
-    });
+   // });
 }
