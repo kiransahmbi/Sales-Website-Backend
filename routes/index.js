9aa7b3c05b12d5f14e5bc6c17257989bc129714d
Kiran Sahmbi <kiransahmbi@yahoo.ca>
2022-08-13 03:06:02 -0400

    Removed Obsolete Index Controller/Routes

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,4 @@ let indexController = require('../controllers/index');
 // Get Homepage
 router.get('/', indexController.home);
 
-// Get About Us
-router.get('/aboutus', indexController.aboutUs);
-
 module.exports = router;
