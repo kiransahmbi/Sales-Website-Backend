64356ad2853ae151c426a9f6701f67c3cabbf7f7
Kiran Sahmbi <kiransahmbi@yahoo.ca>
2022-08-13 00:27:48 -0400

    Removed Public Folder

--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 // Secrect key needs to be edited
-require('dotenv').config();
 
 module.exports = {
-    "ATLASDB": process.env.ATLASDB,
-    "SECRETKEY": process.env.SECRETKEY
+    "ATLASDB": "mongodb+srv://group4Admin:CentennialCollegeSummer2022@cluster003-group4.v8oxwbz.mongodb.net/Group4?retryWrites=true&w=majority",    
+    "SECRETKEY": "y$B&E)H+MbQeThWmZq4t7w!z%C*F-JaNcRfUjXn2r5u8x/A?D(G+KbPeSgVkYp3s"
 }
